35ab7621e5c6a25afa0f0a55fcb8fcdca2f7fc90
Guo Enjing <Enjing.Guo@streamcomputing.com>
2026-02-25 15:43:08 +0800

    初始化redistemplate

--- springboot-demo/src/main/java/com/my/springboot/demo/config/RedisConfig.java
+++ springboot-demo/src/main/java/com/my/springboot/demo/config/RedisConfig.java
@@ -3,24 +3,31 @@ package com.my.springboot.demo.config;
 import java.io.Serializable;
 
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@AutoConfigureAfter(RedisAutoConfiguration.class)
 public class RedisConfig {
 
-	@Bean
-	public RedisTemplate<String, Serializable> redisCacheTemplate(LettuceConnectionFactory redisConnectionFactory) {
-		RedisTemplate<String, Serializable> template = new RedisTemplate<>();
-		template.setKeySerializer(new StringRedisSerializer());
-		template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-		template.setConnectionFactory(redisConnectionFactory);
-		return template;
-	}
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        //序列化
+        JacksonJsonRedisSerializer<Object> fastJsonRedisSerializer = new JacksonJsonRedisSerializer<>(Object.class);
+
+        JdkSerializationRedisSerializer jdkSerializationRedisSerializer = new JdkSerializationRedisSerializer();
+        // value值的序列化采用fastJsonRedisSerializer
+        template.setValueSerializer(fastJsonRedisSerializer);
+        template.setHashValueSerializer(fastJsonRedisSerializer);
+        template.setKeySerializer(jdkSerializationRedisSerializer);
+        template.setHashKeySerializer(jdkSerializationRedisSerializer);
+        return template;
+    }
 }
--- springboot-demo/src/main/java/com/my/springboot/demo/controller/UserController.java
+++ springboot-demo/src/main/java/com/my/springboot/demo/controller/UserController.java
@@ -1,17 +1,27 @@
 package com.my.springboot.demo.controller;
 
-import org.apache.shiro.authz.annotation.RequiresPermissions;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@Slf4j
 @RequestMapping("/user")
-@Controller
+@RestController
 public class UserController {
-    @RequiresPermissions("user:show")
+    @Resource
+    private RedisTemplate redisTemplate;
+
     @ResponseBody
     @RequestMapping("/show")
     public String showUser() {
+        redisTemplate.opsForValue().set("user", "这是学生信息");
+        log.info("这是学生信息");
+        log.info(redisTemplate.opsForValue().get("user").toString());
         return "这是学生信息";
     }
 }
