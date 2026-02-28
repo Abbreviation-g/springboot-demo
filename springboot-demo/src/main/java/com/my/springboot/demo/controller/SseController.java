9b94f7531c52141277f903fcde9a4842ddf46d2e
Guo Enjing <Enjing.Guo@streamcomputing.com>
2026-02-28 16:32:33 +0800

    okhttp结合sse

---
+++ springboot-demo/src/main/java/com/my/springboot/demo/controller/SseController.java
@@ -0,0 +1,126 @@
+package com.my.springboot.demo.controller;
+
+import com.my.springboot.demo.utils.BusinessException;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.ConnectionPool;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.sse.EventSource;
+import okhttp3.sse.EventSourceListener;
+import okhttp3.sse.EventSources;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@RequestMapping("/sse")
+@Controller
+public class SseController {
+    private final ExecutorService executorService = Executors.newCachedThreadPool();
+
+    @GetMapping("/1")
+    public SseEmitter handleSse() {
+        SseEmitter emitter = new SseEmitter();
+        executorService.execute(() -> {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    emitter.send("Message " + i, MediaType.TEXT_PLAIN);
+                    TimeUnit.SECONDS.sleep(1);
+                }
+                emitter.complete();
+            } catch (IOException | InterruptedException e) {
+                emitter.completeWithError(e);
+            }
+        });
+        return emitter;
+    }
+
+    @GetMapping("/2")
+    public SseEmitter handleSse2() {
+        SseEmitter emitter = new SseEmitter();
+        final OkHttpClient okHttpClient = createClient();
+
+        EventSource.Factory factory = EventSources.createFactory(okHttpClient);
+        Request httpRequest = new Request.Builder()
+//                .header("Accept", "audio/pcm")
+                .url("http://127.0.0.1:8763/sse/1")
+                .get()
+                .build();
+        factory.newEventSource(httpRequest, new TTSEventListener(emitter));
+        return emitter;
+    }
+
+    public static OkHttpClient createClient() {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectionPool(new ConnectionPool(30, 100, TimeUnit.SECONDS))
+                .connectTimeout(15, TimeUnit.SECONDS)
+                .writeTimeout(15, TimeUnit.SECONDS)
+                .readTimeout(120, TimeUnit.SECONDS)
+                .build();
+        client.dispatcher().setMaxRequestsPerHost(150);
+        client.dispatcher().setMaxRequests(150);
+        return client;
+    }
+
+
+    private static class TTSEventListener extends EventSourceListener {
+        private SseEmitter emitter;
+
+        TTSEventListener(SseEmitter emitter) {
+            this.emitter = emitter;
+        }
+
+        @Override
+        public void onOpen(EventSource eventSource, Response response) {
+            log.info("onOpen");
+            try {
+                emitter.send("onOpen");
+            } catch (IOException e) {
+                throw new BusinessException(HttpStatus.INTERNAL_SERVER_ERROR.value(), "onOpen error");
+            }
+        }
+
+        @Override
+        public void onEvent(EventSource eventSource, String id, String type, String data) {
+            log.info("onEvent, data={}", data);
+            try {
+                emitter.send(data);
+            } catch (IOException e) {
+                throw new BusinessException(HttpStatus.INTERNAL_SERVER_ERROR.value(), "onEvent error");
+            }
+        }
+
+        @Override
+        public void onClosed(EventSource eventSource) {
+            log.info("onClosed");
+            try {
+                emitter.send("onClosed");
+            } catch (IOException e) {
+                throw new BusinessException(HttpStatus.INTERNAL_SERVER_ERROR.value(), "onClosed error");
+            } finally {
+                emitter.complete();
+            }
+        }
+
+        @Override
+        public void onFailure(EventSource eventSource, Throwable t, Response response) {
+            log.error("onFailure", t);
+            try {
+                emitter.send("onFailure");
+            } catch (IOException e) {
+                throw new BusinessException(HttpStatus.INTERNAL_SERVER_ERROR.value(), "onFailure error");
+            } finally {
+                emitter.completeWithError(t);
+            }
+        }
+    }
+}
