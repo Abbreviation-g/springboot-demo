2bc5d0cbbd894b76c77a9f1e2c46c570ad6cdca9
Guo Enjing <Enjing.Guo@streamcomputing.com>
2026-03-17 11:29:45 +0800

    开启定时任务

--- springboot-demo/src/main/java/com/my/springboot/demo/SpringBootDemoApplication.java
+++ springboot-demo/src/main/java/com/my/springboot/demo/SpringBootDemoApplication.java
@@ -2,10 +2,12 @@ package com.my.springboot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class SpringBootDemoApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
 }
---
+++ springboot-demo/src/main/java/com/my/springboot/demo/config/CustomSchedule.java
@@ -0,0 +1,21 @@
+package com.my.springboot.demo.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Slf4j
+@Component
+public class CustomSchedule {
+
+    /**
+     * 每1分钟执行一次
+     */
+    @Scheduled(cron = "0 0/1 * * * ?")
+    public void run() {
+        log.info("定时任务开始执行... date:{}", new Date());
+        System.out.println("定时任务开始执行...");
+    }
+}
